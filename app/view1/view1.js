76b7ca4b766ea725bde3ee8b6b8621a8e2e7068a
meg-m <meghamaneck123@gmail.com>
2017-07-15 08:21:36 +0530

    Add logic to take details of people

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,5 +1,4 @@
 'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +8,44 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+
+.service('friendService', function($http){
+	function getFriendNames() 
+	{
+		var promise = $http({ method: 'GET', url: 'json/friends.json', cache : true });
+		return promise;
+	}
+	return {getFriendNamesJson: getFriendNames}
+})
+
+
+.controller('View1Ctrl', [ '$scope', '$location', 'friendService', function($scope, $location, friendService) {
+
+friendService.getFriendNamesJson()
+.then(function success(response) {
+	$scope.friends = response.data;
+	$scope.noOfFriends = response.data.length;
+});
+
+
+var vm = this;
+vm.navigateToCreate = function() {	
+    $location.path('/view2');
+}
+
+$scope.showTable = false;
+$scope.addNumber = function() {
+	$scope.showTable = true;
+}
+
+$scope.addData = function() {
+
+//$scope.friends.push( {'name':$scope.name} );
+//$scope.friends.push( {'from':$scope.from, 'to':$scope.to, 'amt': $scope.amt} );
+//$scope.name = '';
+vm.navigateToCreate();
+};
+
+
 
 }]);
